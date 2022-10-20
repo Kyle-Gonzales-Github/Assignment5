e3125d2ca851f7d479fd4e8b33c0b99389bbfe89
Kyle-Gonzales-Github <114026564+Kyle-Gonzales-Github@users.noreply.github.com>
2022-10-20 12:08:02 -0400

    Final Push

--- src/App.java
+++ src/App.java
@@ -1,20 +1,26 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.LineNumberReader;
-import java.util.Scanner;
 
 public class App {
+
+    private static int numberOfLines;
     public static void main(String[] args) throws Exception {
         File f = new File("src/words.txt");
         try {
             FileReader fr = new FileReader(f);
             LineNumberReader lnr = new LineNumberReader(fr);
             lnr.skip(Long.MAX_VALUE);
-            System.out.println("Number of words in file words.txt: " + lnr.getLineNumber());
+            numberOfLines = (lnr.getLineNumber() + 1);
+            System.out.println("Outputted number of lines to output.txt");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        File file = new File("src/output.txt");
+        FileWriter fileWriter = new FileWriter(file);
+        fileWriter.write("Number of lines: " + numberOfLines);
+        fileWriter.close();
     }
 }
